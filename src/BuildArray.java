cc53905a29eae0d5ed15a3f006c173923f763340
Servachak <servachak088@gmail.com>
2017-08-05 11:41:48 +0300

    Пользователь вводит с клавиатуры натуральное число большее 3, которое сохраняется в переменную n. Если пользователь ввёл не подходящее число, то программа должна просить пользователя повторить ввод. Создать массив из n случайных целых чисел из отрезка [0;n] и вывести его на экран. Создать второй массив только из чётных элементов первого массива, если они там есть, и вывести его на экран.

--- src/BuildArray.java
+++ src/BuildArray.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 public class BuildArray {
 
    static Scanner scanner  = new Scanner(System.in);
+
+/** number число яке вказує на довжине масиву та вибірку **/
+
     public int[] arrayBuild(int number){
 
         int [] arr = new int[number];
@@ -21,19 +24,25 @@ public class BuildArray {
     }
 
     public int auditNamber(){
+        /** okNumber - змінна для перевірки істиності умови **/
         boolean okNumber = false;
+
+        /** number  - число введене від користувача**/
         int number = 0;
 
        while (!okNumber){
 
             System.out.println("Введіть число більше трьох");
 
+            /** користувач вводить число **/
              number = BuildArray.scanner.nextInt();
 
+             /** якщо число введене користувачем більше 3 то виконуються умова**/
             if (number > 3){
 
                 okNumber = true;
 
+                /** якщо ні то виводиться повідомлення **/
             }else{
                 System.out.println("Число має бути більше трьох");
 
@@ -42,11 +51,15 @@ public class BuildArray {
         return number;
         }
 
+        /** arr[] - вхідний масив**/
         public void evenNumbersArray(int arr[]){
 
+        /** створюється новий масив який дорівнює довжині вхідного масиву **/
         int [] newArray = new int[arr.length];
         for (int i = 0; i < arr.length; i++){
 
+            /** пробігаючись по вхідному масиву перевіряємо чи елемент масиву при діленні по модулю на 2 ріввний 0(тобто чи парний)
+            * якщо так то зберігаємо це значення в новий масив **/
             if (arr[i] % 2 == 0 && arr[i] != 0){
                 newArray[i] = arr[i];
                 System.out.print(newArray[i] + " ");
